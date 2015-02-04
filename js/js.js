b7c83210f9e722f4690b87dc53611301a49654c3
Federica Galli <jastra89@gmail.com>
2015-02-04 23:50:16 +1100

    editing of js

--- js/js.js
+++ js/js.js
@@ -1,28 +1,13 @@
 $(document).ready(function(){
 
-	heightTopbar();
 	heightSlider();
 	
 	$(window).resize(function() {
-		heightTopbar();
 		heightSlider();
 	});
 	
 });
 
-function heightTopbar() {
-	$("#badge-left").css("height", $("#badge-left").width()+"px");
-	if( $("#badge-left").width() >= 200 ) {
-		$("#top").css("width", $(window).width()-201-$("#image-right").width()+"px");
-		$("#left_content").css("width", $(window).width()-201-$("#image-right").width()+"px");
-	} else {
-		$("#top").css("width", "70%");
-		$("#left_content").css("width", "70%");
-	}
-	$("#top").css("height", $("#badge-left").width()+"px");
-	$("#image-right").css("height"
-	, $("#image-right").width()+"px");
-}
 
 
 function heightSlider() {
